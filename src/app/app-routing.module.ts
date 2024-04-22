97554578148437f4e47a51a3cf33e688badb7c60
DiegoMiguel621 <130955808+DiegoMiguel621@users.noreply.github.com>
2024-04-22 03:06:04 -0600

    login, header, rutas
    se crearon ya los componentes header, footer, login y recuperar-contra
    se crearon sus respectivas rutas
    se hizo dise;o de login a como ya estaba y del nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RecuperarContraComponent } from './components/recuperar-contra/recuperar-contra.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'login', component:LoginComponent},
+  {path:'header',component:HeaderComponent},
+  {path:'footer',component:FooterComponent},
+  {path:'recu',component:RecuperarContraComponent},
+  {path:'**',redirectTo:'login',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RecuperarContraComponent } from './components/recuperar-contra/recuperar-contra.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    RecuperarContraComponent
   ],
   imports: [
     BrowserModule,
