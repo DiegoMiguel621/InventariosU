f77d588de60423645a1aa6ca54bf5465e23b3285
DiegoMiguel621 <130955808+DiegoMiguel621@users.noreply.github.com>
2025-01-15 16:52:09 -0600

    paginacion de la tabla

--- src/app/components/personal/personal.component.ts
+++ src/app/components/personal/personal.component.ts
@@ -4,8 +4,7 @@ import { ModalAgregarPersonalComponent } from '../../modal-agregar-personal/moda
 import { ModalVerPersonalComponent } from '../../modal-ver-personal/modal-ver-personal.component';
 import { ModalEditarPersonalComponent } from '../../modal-editar-personal/modal-editar-personal.component';
 import { ModalEliminarPersonalComponent } from '../../modal-eliminar-personal/modal-eliminar-personal.component';
-
-import { TrabajadoresService } from '../../service/trabajadores.service'; // Importamos el nuevo servicio
+import { TrabajadoresService } from '../../service/trabajadores.service';
 
 @Component({
   selector: 'app-personal',
@@ -13,92 +12,100 @@ import { TrabajadoresService } from '../../service/trabajadores.service'; // Imp
   styleUrls: ['./personal.component.css'],
 })
 export class PersonalComponent implements OnInit {
-  trabajadores: any[] = []; // Cambiamos 'bienes' por 'trabajadores'
+  trabajadores: any[] = [];
+  paginatedTrabajadores: any[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+
+  Math = Math; // Exponemos el objeto Math al template
 
   constructor(
     private _matDialog: MatDialog,
-    private trabajadoresService: TrabajadoresService // Inyectamos el nuevo servicio
+    private trabajadoresService: TrabajadoresService
   ) {}
 
   ngOnInit(): void {
-    // Usamos el servicio TrabajadoresService para obtener los datos de la API
     this.trabajadoresService.getTrabajadores().subscribe((data) => {
       this.trabajadores = data;
+      this.updatePagination();
     });
+  }
+
+  updatePagination(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedTrabajadores = this.trabajadores.slice(startIndex, endIndex);
+  }
+
+  nextPage(): void {
+    if (this.currentPage * this.itemsPerPage < this.trabajadores.length) {
+      this.currentPage++;
+      this.updatePagination();
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePagination();
+    }
   }
 
   agregarPersonal(): void {
     const dialogRef = this._matDialog.open(ModalAgregarPersonalComponent);
     dialogRef.afterClosed().subscribe((result) => {
-      if (result) { // Si se recibe un valor "true"
-        console.log('El trabajador fue agregado, actualizando la tabla...');
+      if (result) {
         this.trabajadoresService.getTrabajadores().subscribe((data) => {
           this.trabajadores = data;
+          this.updatePagination();
         });
-      } else {
-        console.log('El modal fue cerrado sin agregar un trabajador.');
       }
     });
   }
-    
-
-
 
   verrPersonal(id: number): void {
     const dialogRef = this._matDialog.open(ModalVerPersonalComponent, {
-      data: { id }, // Pasamos el ID al modal
+      data: { id },
     });
 
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        // Refrescamos la lista de trabajadores al cerrar el modal
         this.trabajadoresService.getTrabajadores().subscribe((data) => {
           this.trabajadores = data;
+          this.updatePagination();
         });
       }
     });
   }
-
 
   editarPersonal(trabajador: any): void {
     const dialogRef = this._matDialog.open(ModalEditarPersonalComponent, {
-      data: trabajador, // Asegúrate de que "trabajador" tiene el campo idTrabajador
+      data: trabajador,
     });
-  
+
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         this.trabajadoresService.getTrabajadores().subscribe((data) => {
           this.trabajadores = data;
+          this.updatePagination();
         });
       }
     });
   }
-  
-  
-  
-  
-  
-
 
   eliminarPersonal(id: number): void {
-    console.log('ID del trabajador a eliminar:', id); // Confirma que el ID es correcto
     const dialogRef = this._matDialog.open(ModalEliminarPersonalComponent, {
-      data: { id } // Pasamos el ID al modal
+      data: { id },
     });
-  
+
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         this.trabajadoresService.getTrabajadores().subscribe((data) => {
           this.trabajadores = data;
+          this.updatePagination();
         });
       }
     });
   }
-  
-  
-
 }
-
-
-
 
