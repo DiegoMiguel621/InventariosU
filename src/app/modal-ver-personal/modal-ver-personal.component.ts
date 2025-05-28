c5f34731366807bcb7b466843daf6fb5c2b3e5f9
DiegoMiguel621 <130955808+DiegoMiguel621@users.noreply.github.com>
2025-05-28 04:02:13 -0600

    asignacion de bienes a resguardantes
    tabla de bienes que posee un resguardante

--- src/app/modal-ver-personal/modal-ver-personal.component.ts
+++ src/app/modal-ver-personal/modal-ver-personal.component.ts
@@ -3,8 +3,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TrabajadoresService } from '../service/trabajadores.service';
-
-
+import { BienesService } from '../service/bienes.service';
 
 @Component({
   selector: 'app-modal-ver-personal',
@@ -13,9 +12,11 @@ import { TrabajadoresService } from '../service/trabajadores.service';
 })
 export class ModalVerPersonalComponent implements OnInit {
   trabajador: any = null; // Almacena los datos del trabajador
+  bienesAsignados: any[] = [];  // lista de bienes
 
   constructor(
     private trabajadoresService: TrabajadoresService,
+    private bienesService: BienesService, 
     public dialogRef: MatDialogRef<ModalVerPersonalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { id: number } // Recibimos el ID
   ) {}
@@ -25,12 +26,33 @@ export class ModalVerPersonalComponent implements OnInit {
     this.trabajadoresService.getTrabajadorById(this.data.id).subscribe(
       (data) => {
         this.trabajador = data; // Asignamos los datos
+        this.cargarBienesAsignados();
       },
       (error) => {
         console.error('Error al obtener los datos del trabajador:', error);
       }
     );
   }
+
+  private cargarBienesAsignados(): void {
+  this.bienesService.getBienes()
+    .subscribe(bienes => {
+      const nombreTrab = (this.trabajador.nombre || '')
+        .trim()
+        .toLowerCase();
+
+      this.bienesAsignados = bienes.filter(b => {
+        const nomRes = (b.nomRes || '')
+          .trim()
+          .toLowerCase();
+        return nomRes === nombreTrab;
+      });
+
+      console.log('Comparando:', nombreTrab, 'contra lista de nomRes:', 
+                  bienes.map(b => (b.nomRes||'').trim().toLowerCase()));
+    });
+}
+
 
   closeModal(): void {
     this.dialogRef.close();
